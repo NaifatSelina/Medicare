f4b57c1afe2eef4d770a33008c045a82519aa56c
NaifatSelina <naifatselina@icloud.com>
2023-12-08 21:00:00 +0000

    create routing in app.js adding routing for components

--- my-medicare-frontend/src/App.js
+++ my-medicare-frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Registration from './components/Registration';
+import MedicineList from './components/MedicineList';
+import Cart from './components/Cart';
+import OrderSummary from './components/OrderSummary';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/registration" component={Registration} />
+          <Route path="/medicines" component={MedicineList} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/order-summary" component={OrderSummary} />
+          {/* Add more routes as needed */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
