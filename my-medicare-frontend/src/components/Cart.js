5602c37247550e4b7b7f2d8bf4532ea4c96d03cf
NaifatSelina <naifatselina@icloud.com>
2023-12-08 22:04:40 +0000

    further style cart

--- my-medicare-frontend/src/components/Cart.js
+++ my-medicare-frontend/src/components/Cart.js
@@ -1,13 +1,104 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+// Styles
+const navStyle = {
+  backgroundColor: '#f8f9fa',
+  padding: '10px 0',
+};
+
+const containerStyle = {
+  maxWidth: '1200px',
+  margin: '0 auto',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+};
+
+const brandStyle = {
+  textDecoration: 'none',
+  fontSize: '24px',
+  color: '#007bff',
+};
+
+const navLinksStyle = {
+  display: 'flex',
+  gap: '20px',
+};
+
+const cartContainerStyle = {
+  maxWidth: '1200px',
+  margin: '0 auto',
+  padding: '20px',
+};
+
+const cartHeaderStyle = {
+  fontSize: '32px',
+  color: '#007bff',
+  textAlign: 'center',
+  marginBottom: '30px',
+};
+
+const medicineListStyle = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  gap: '20px',
+  justifyContent: 'center',
+};
+
+const medicineCardStyle = {
+  border: '1px solid #ddd',
+  padding: '20px',
+  borderRadius: '8px',
+  textAlign: 'center',
+  maxWidth: '300px',
+};
+
+const medicineImageStyle = {
+  width: '100%',
+  marginBottom: '20px',
+};
+
+const addToCartButtonStyle = {
+  padding: '10px 20px',
+  fontSize: '16px',
+  backgroundColor: '#007bff',
+  color: '#fff',
+  border: 'none',
+  cursor: 'pointer',
+};
+
+const cartItemsStyle = {
+  marginTop: '30px',
+};
+
+const removeFromCartButtonStyle = {
+  marginLeft: '10px',
+  padding: '5px 10px',
+  fontSize: '14px',
+  backgroundColor: '#dc3545',
+  color: '#fff',
+  border: 'none',
+  cursor: 'pointer',
+};
+
+const footerStyle = {
+  backgroundColor: '#343a40',
+  color: '#fff',
+  padding: '20px 0',
+  position: 'fixed',
+  width : '100%',
+  bottom: 0,
+  textAlign: 'center',
+  
+};
+
+// Component
 const Cart = () => {
   const [cartItems, setCartItems] = useState([]);
 
   const medicineData = [
-    { id: 1, name: 'Medicine 1', price: 10.99 },
-    { id: 2, name: 'Medicine 2', price: 19.99 },
-    { id: 3, name: 'Medicine 3', price: 5.99 },
+    // ... (unchanged)
   ];
 
   const handleAddToCart = (medicine) => {
@@ -21,94 +112,63 @@ const Cart = () => {
   };
 
   return (
-    <div style={cartContainerStyle}>
-      <h2 style={cartHeaderStyle}>Your Cart</h2>
-      <div style={medicineListStyle}>
-        {medicineData.map((medicine, index) => (
-          <div key={medicine.id} style={medicineCardStyle}>
-            <h3>{medicine.name}</h3>
-            <p>${medicine.price.toFixed(2)}</p>
-            <button onClick={() => handleAddToCart(medicine)} style={addToCartButtonStyle}>
-              Add to Cart
-            </button>
+    <div>
+      {/* Header */}
+      <nav style={navStyle}>
+        <div style={containerStyle}>
+          <a href="#" style={brandStyle}>
+            Medicare
+          </a>
+          <div style={navLinksStyle}>
+          <a href="#">Home</a>
+            <a href="#">Products</a>
+            <a href="#">About Us</a>
+            <a href="#">Login</a>
           </div>
-        ))}
+        </div>
+      </nav>
+
+      {/* Cart Container */}
+      <div style={cartContainerStyle}>
+        <h2 style={cartHeaderStyle}>Your Cart</h2>
+        <div style={medicineListStyle}>
+          {medicineData.map((medicine, index) => (
+            <div key={medicine.id} style={medicineCardStyle}>
+              <img src={medicine.image} alt={medicine.name} style={medicineImageStyle} />
+              <h3>{medicine.name}</h3>
+              <p>${medicine.price.toFixed(2)}</p>
+              <p>{medicine.description}</p>
+              <button onClick={() => handleAddToCart(medicine)} style={addToCartButtonStyle}>
+                Add to Cart
+              </button>
+            </div>
+          ))}
+        </div>
+        <div style={cartItemsStyle}>
+          <h3>Items in Cart:</h3>
+          {cartItems.length === 0 ? (
+            <p>Your cart is empty.</p>
+          ) : (
+            <ul>
+              {cartItems.map((item, index) => (
+                <li key={index}>
+                  {item.name} - ${item.price.toFixed(2)}{' '}
+                  <button onClick={() => handleRemoveFromCart(index)} style={removeFromCartButtonStyle}>
+                    Remove
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
-      <div style={cartItemsStyle}>
-        <h3>Items in Cart:</h3>
-        {cartItems.length === 0 ? (
-          <p>Your cart is empty.</p>
-        ) : (
-          <ul>
-            {cartItems.map((item, index) => (
-              <li key={index}>
-                {item.name} - ${item.price.toFixed(2)}{' '}
-                <button onClick={() => handleRemoveFromCart(index)} style={removeFromCartButtonStyle}>
-                  Remove
-                </button>
-              </li>
-            ))}
-          </ul>
-        )}
+
+      {/* Footer */}
+      <div style={footerStyle}>
+        <p>&copy; 2023 Medicare. All rights reserved.</p>
       </div>
     </div>
   );
-};
-
-// Styles
-const cartContainerStyle = {
-  maxWidth: '800px',
-  margin: '0 auto',
-  padding: '20px',
-};
-
-const cartHeaderStyle = {
-  textAlign: 'center',
-  fontSize: '28px',
-  color: '#333',
-  marginBottom: '20px',
-};
-
-const medicineListStyle = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  flexWrap: 'wrap',
-};
-
-const medicineCardStyle = {
-  width: '200px',
-  border: '1px solid #ddd',
-  borderRadius: '8px',
-  padding: '10px',
-  marginBottom: '20px',
-  textAlign: 'center',
-};
-
-const addToCartButtonStyle = {
-  backgroundColor: '#007bff',
-  color: '#fff',
-  padding: '8px',
-  fontSize: '14px',
-  border: 'none',
-  borderRadius: '4px',
-  cursor: 'pointer',
-  marginTop: '10px',
-};
-
-const removeFromCartButtonStyle = {
-  backgroundColor: '#dc3545',
-  color: '#fff',
-  padding: '8px',
-  fontSize: '14px',
-  border: 'none',
-  borderRadius: '4px',
-  cursor: 'pointer',
-  marginTop: '10px',
-  marginLeft: '10px',
-};
-
-const cartItemsStyle = {
-  marginTop: '40px',
 };
 
 export default Cart;
